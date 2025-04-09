20b7d1ef8f2810ff60b549f76da303d975d99034
clmntd <clement.dai@gmail.com>
2025-04-09 13:24:52 +1000

    Added endpoint check
    - Checks if user exists and is a facilitator

--- server/controllers/auctionController.js
+++ server/controllers/auctionController.js
@@ -2,44 +2,51 @@ const pool = require('../config/db');
 
 //Create auction
 const createAuction = async (req, res) => {
-  const { facilitator_id } = req.body;
-  try {
-      const result = await pool.query(
-          'insert into auctions (facilitator_id, status) values ($1, $2)',
-          [facilitator_id, 'open']
-      );
-      res.json({ message: `Auction created!` });
-      console.log('Auction created:', result.rows[0]);
-      return result.rows[0];
-  } catch (error) {
-      console.error('Error creating auction:', err);
-  }
+    const { facilitator_id } = req.body;
+    const user = await pool.query('select * from users where id = $1', [facilitator_id]);
+    if (user.rows.length === 0) {
+        return res.status(400).json({ message: 'User not found' });
+    }
+    if (user.rows[0].role === 'supplier') {
+        return res.status(400).json({ message: 'User is not a facilitator' });
+    }
+    try {
+        const result = await pool.query(
+            'insert into auctions (facilitator_id, status) values ($1, $2)',
+            [facilitator_id, 'open']
+        );
+        res.json({ message: `Auction created!` });
+        console.log('Auction created:', result.rows[0]);
+        return result.rows[0];
+    } catch (error) {
+        console.error('Error creating auction:', err);
+    }
 };
 
 //Delete an auction
 const deleteAuction = async (req, res) => {
-  const { userId } = req.body;
-  const user = await pool.query('select * from users where id = $1', [userId]);
-  if (user.rows.length === 0) {
-      return res.status(400).json({ message: 'User not found' });
-  }
-  if (user.rows[0].role === 'supplier') {
-      return res.status(400).json({ message: 'User is not a facilitator' });
-  }
-  const { id } = req.params;
-  const auction = await pool.query('select * from auctions where id = $1', [id]);
-  if (auction.rows.length === 0) {
-      return res.status(400).json({ message: 'Auction not found' });
-  }
-  try {
-      await pool.query('delete from auctions where id = $1;', [id]);
-      await pool.query('delete from bids where auction_id = $1;', [id]);
-      console.log('Auction routes delete /:id');
-      return res.status(200).json({ message: `Auction ${id} deleted successfully` });
-  } catch (err) {
-      console.error('Error deleting auctions:', err);
-      res.status(500).json({ error: 'Error deleting auction' });
-  }
+    const { userId } = req.body;
+    const user = await pool.query('select * from users where id = $1', [userId]);
+    if (user.rows.length === 0) {
+        return res.status(400).json({ message: 'User not found' });
+    }
+    if (user.rows[0].role === 'supplier') {
+        return res.status(400).json({ message: 'User is not a facilitator' });
+    }
+    const { id } = req.params;
+    const auction = await pool.query('select * from auctions where id = $1', [id]);
+    if (auction.rows.length === 0) {
+        return res.status(400).json({ message: 'Auction not found' });
+    }
+    try {
+        await pool.query('delete from auctions where id = $1;', [id]);
+        await pool.query('delete from bids where auction_id = $1;', [id]);
+        console.log('Auction routes delete /:id');
+        return res.status(200).json({ message: `Auction ${id} deleted successfully` });
+    } catch (err) {
+        console.error('Error deleting auctions:', err);
+        res.status(500).json({ error: 'Error deleting auction' });
+    }
 };
 
 //Get all auctions
@@ -50,23 +57,23 @@ const getAuctions = async (req, res) => {
 
 //Get all auctions user has participated in
 const getParticipate = async (req, res) => {
-  const { id } = req.params;
+    const { id } = req.params;
 
-  const auctions = await pool.query(
-      'select auction_id, bids.id AS bid_id, price, quality, facilitator_id, supplier_id, users.name as name from auctions inner join bids ON bids.auction_id = auctions.id inner join users on users.id = bids.supplier_id where auctions.id in (select distinct auction_id from bids where supplier_id = $1) order by auction_id asc, bids.id asc', [id]);
-  console.log('Auction routes get /:id auctions.rows: ', auctions.rows);
-  if (auctions.rows.some(x => x.supplier_id == id)) {
-      return res.json({ auctions: auctions.rows });
-  } else {
-      return res.json({ auctions: [] });
-  }
+    const auctions = await pool.query(
+        'select auction_id, bids.id AS bid_id, price, quality, facilitator_id, supplier_id, users.name as name from auctions inner join bids ON bids.auction_id = auctions.id inner join users on users.id = bids.supplier_id where auctions.id in (select distinct auction_id from bids where supplier_id = $1) order by auction_id asc, bids.id asc', [id]);
+    console.log('Auction routes get /:id auctions.rows: ', auctions.rows);
+    if (auctions.rows.some(x => x.supplier_id == id)) {
+        return res.json({ auctions: auctions.rows });
+    } else {
+        return res.json({ auctions: [] });
+    }
 };
 
 
 
-module.exports = { 
-  createAuction, 
-  deleteAuction,
-  getAuctions,
-  getParticipate
+module.exports = {
+    createAuction,
+    deleteAuction,
+    getAuctions,
+    getParticipate
 };
