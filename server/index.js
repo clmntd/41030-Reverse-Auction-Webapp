fc52438913f1bdcb659d208c6b666296d4241a2f
clmntd <clement.dai@gmail.com>
2025-04-15 23:10:25 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,6 +17,11 @@ app.get('/', (req, res) => {
   res.send('Auction API is running');
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build'));
+});
+
+
 //heroku
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
