c938fb9a9056fa9fe7a9902be410177b0f83c0ac
clmntd <clement.dai@gmail.com>
2025-04-15 22:21:16 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/build')))
-if(process.env.NODE_ENV --- 'production'){
+if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '../client/build')))
 }
 console.log(path.join(__dirname, '../client/build'));
