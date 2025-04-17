de16706aaf441ce4bdf643831ac955f5141cb6c6
clmntd <clement.dai@gmail.com>
2025-04-17 16:30:28 +1000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ if(process.env.NODE_ENV === 'production'){
 }
 console.log(path.join(__dirname, '../client/build'));
 
-app.get('/', (req, res) => {
-  res.send('Auction API is running');
-});
+// app.get('/', (req, res) => {
+//   res.send('Auction API is running');
+// });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
